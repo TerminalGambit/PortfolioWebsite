9e13177dd79f29ea93d9b17a842d7c59beb568d6
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-06 19:11:29 +0100

    Update Header component to display the user's name in the portfolio title

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -3,8 +3,8 @@
 import React from 'react';
 import './Header.css';
 
-function Header() {
-  return <header><h1>My Portfolio</h1></header>;
+function Header({ name }) {
+    return <header><h1>{name}'s Portfolio</h1></header>;
 }
 
 export default Header;
