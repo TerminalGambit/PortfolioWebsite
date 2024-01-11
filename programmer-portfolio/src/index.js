85758742fd4b2890e0c548ba50f3cb28d2e11c8d
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-12 00:57:41 +0100

    Refactor React.StrictMode in index.js

--- programmer-portfolio/src/index.js
+++ programmer-portfolio/src/index.js
@@ -1,14 +1,15 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // Assuming you have global styles defined in index.css
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
