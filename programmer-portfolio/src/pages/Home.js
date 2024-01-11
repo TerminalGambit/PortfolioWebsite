206ca0b69b8bd26afd990c8f099e4d32c151de60
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-12 00:39:26 +0100

    Refactor App.js and index.js

---
+++ programmer-portfolio/src/pages/Home.js
@@ -0,0 +1,13 @@
+// Example for Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to My Portfolio</h2>
+            {/* Additional content will go here */}
+        </div>
+    );
+};
+
+export default Home;
